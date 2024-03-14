deaf116a5638bdce6aac81ede55c5b82115063f2
Matthew Mintz <Matthewmintz0818@gmail.com>
2024-03-13 19:41:19 -0500

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Lili Fiano on 3/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}sss
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/Fetch_Food_App_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_Food_App_DemoApp.swift
+//  Shared
+//
+//  Created by Lili Fiano on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_Food_App_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
